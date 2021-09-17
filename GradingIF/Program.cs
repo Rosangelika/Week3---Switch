d8d8bee02f887a0c2fa77c01a3349406a8cd9df6
Rosangelika Todoruk <rtodoruk@tktk.ee>
2021-09-18 00:47:50 +0300

    add new projects

---
+++ GradingIF/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GradingIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai;
+            //kui kasutaja sai 'A', konsool kuvab "Suurepärane!";
+            //kui kasutaja sai 'B', konsool kuvab "Väga hea!";
+            //kui kasutaja sai 'C', konsool kuvab "Hea!"; 
+            //kui kasutaja sai 'D', konsool kuvab "Rahuldav!";
+            //kui kasutaja sai 'E', konsool kuvab "Kasin!";
+            //kui kasutaja sai 'F', konsool kuvab "Puudulik!";
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (userResult == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            else if (userResult == 'B')
+            {
+                Console.WriteLine("Väga hea!");
+            }
+            else if (userResult == 'C')
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if (userResult == 'D')
+            {
+                Console.WriteLine("Rahuldav!");
+            }
+            if (userResult == 'E')
+            {
+                Console.WriteLine("Kasin!");
+            }
+            if (userResult == 'F')
+            {
+                Console.WriteLine("Puudulik");
+            }
+
+            Console.WriteLine("Edukate õpingute jätku!");
+        }
+        
+    }
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai;
+            //kui kasutaja sai 'A', konsool kuvab "Suurepärane!";
+            //kui kasutaja sai 'B', konsool kuvab "Väga hea!";
+            //kui kasutaja sai 'C', konsool kuvab "Hea!"; 
+            //kui kasutaja sai 'D', konsool kuvab "Rahuldav!";
+            //kui kasutaja sai 'E', konsool kuvab "Kasin!";
+            //kui kasutaja sai 'F', konsool kuvab "Puudulik!";
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (userResult)
+            {
+                case 'A':  
+                    Console.WriteLine("Suurepärane!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Väga hea!");
+                    break;
+                case 'C':
+                    Console.WriteLine("Hea!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Rahuldav!");
+                    break;
+                case 'E':
+                    Console.WriteLine("Kasin!");
+                    break;
+                case 'F':
+                    Console.WriteLine("Puudulik!");
+                    break;
+
+            }
+            Console.WriteLine("Edukate õpingute jätku!");
+        }
+    }
+}
